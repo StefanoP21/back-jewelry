e5306158dc06bcf73bc3d285776f8c02b53f4039
Stefano Palomino <stefanop21@outlook.es>
2024-09-23 22:14:23 -0500

    feat: fixed eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,64 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import _import from 'eslint-plugin-import';
+import prettier from 'eslint-plugin-prettier';
+import { fixupPluginRules } from '@eslint/compat';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all
+});
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    ignores: [
-      'node_modules',
-      'dist',
-      'coverage',
-      '.husky',
-      'commitlint.config.js',
-    ],
-  },
+	{
+		ignores: [
+			'test/',
+			'node_modules/',
+			'dist/',
+			'coverage/',
+			'.husky/',
+			'commitlint.config.js',
+			'jest.config.ts',
+			'eslint.config.mjs'
+		]
+	},
+	...compat.extends('plugin:prettier/recommended', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'),
+	{
+		plugins: {
+			'@typescript-eslint': typescriptEslint,
+			import: fixupPluginRules(_import),
+			prettier
+		},
+
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+
+			parserOptions: {
+				project: './tsconfig.json'
+			}
+		},
+
+		rules: {
+			'prettier/prettier': 'error',
+			camelcase: 'error',
+			'spaced-comment': 'error',
+			quotes: ['error', 'single'],
+			'no-duplicate-imports': 'error',
+			'no-unused-vars': 'off',
+			'no-magic-numbers': 'off',
+			'@typescript-eslint/no-unused-vars': 'error',
+			'@typescript-eslint/explicit-function-return-type': 'error',
+			'@typescript-eslint/strict-boolean-expressions': 'off',
+			'@typescript-eslint/no-extraneous-class': 'off',
+			'@typescript-eslint/no-magic-numbers': 'error',
+			'@typescript-eslint/no-unsafe-argument': 'off'
+		}
+	}
 ];
