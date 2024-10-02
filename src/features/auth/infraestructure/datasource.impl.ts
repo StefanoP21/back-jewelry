460fbe68bb5e3588b78d84df458dc1c785a652e6
Stefano Palomino <stefanop21@outlook.es>
2024-10-02 00:09:32 -0500

    refactor(auth): updated datasource impl logic for prisma

--- src/features/auth/infraestructure/datasource.impl.ts
+++ src/features/auth/infraestructure/datasource.impl.ts
@@ -1,67 +1,62 @@
-// import { BcryptAdapter, CustomError, envs, JwtAdapter } from '../../../core';
-// import { AuthEntity, UserEntity, type AuthDatasource, type LoginUserDto, type RegisterUserDto } from '../domain';
+import { BcryptAdapter, CustomError, envs, JwtAdapter } from '../../../core';
+import { prisma } from '../../../data/postgresql';
+import { AuthEntity, UserEntity, type AuthDatasource, type LoginUserDto, type RegisterUserDto } from '../domain';
 
-// export class AuthDatasourceImpl implements AuthDatasource {
-// 	private async handleToken(id: string): Promise<string> {
-// 		const jwtAdapter = new JwtAdapter(envs.JWT_SEED);
-// 		const token = await jwtAdapter.generateToken<string>({ id });
+export class AuthDatasourceImpl implements AuthDatasource {
+	private async handleToken(dni: string): Promise<string> {
+		const jwtAdapter = new JwtAdapter(envs.JWT_SEED);
+		const token = await jwtAdapter.generateToken<string>({ dni });
 
-// 		if (!token) throw CustomError.internalServer('Error al generar el token');
+		if (!token) throw CustomError.internalServer('Error al generar el token');
 
-// 		return token;
-// 	}
+		return token;
+	}
 
-// 	async register(dto: RegisterUserDto): Promise<AuthEntity> {
-// 		const existUser = await UserModel.findOne({ email: dto.email });
-// 		if (existUser)
-// 			throw CustomError.badRequest('El correo se encuentra registrado', [
-// 				{ constraint: 'User already exist', fields: ['email'] }
-// 			]);
+	async register(dto: RegisterUserDto): Promise<AuthEntity> {
+		const existUser = await prisma.user.findFirst({ where: { dni: dto.dni } });
+		if (existUser)
+			throw CustomError.badRequest('Usuario ya registrado', [{ constraint: 'El dni es único', fields: ['dni'] }]);
 
-// 		try {
-// 			const user = new UserModel(dto);
+		const user = await prisma.user.create({ data: dto });
 
-// 			//* encrypt password
-// 			user.password = BcryptAdapter.hash(dto.password);
+		//* encrypt password
+		user.password = BcryptAdapter.hash(dto.password);
 
-// 			await user.save();
+		await prisma.user.update({ where: { id: user.id }, data: { password: user.password } });
 
-// 			const { password, ...rest } = UserEntity.fromObject(user);
+		const { password, ...rest } = UserEntity.fromObject(user);
 
-// 			//* jwt
-// 			const token = await this.handleToken(user.id);
+		//* jwt
+		const token = await this.handleToken(user.dni);
 
-// 			return new AuthEntity(rest, token);
-// 		} catch (error) {
-// 			throw CustomError.internalServer('Internal server error');
-// 		}
-// 	}
+		return new AuthEntity(rest, token);
+	}
 
-// 	async login(dto: LoginUserDto): Promise<AuthEntity> {
-// 		const user = await UserModel.findOne({ email: dto.email });
-// 		if (!user) throw CustomError.badRequest('Usuario no encontrado con este correo');
+	async login(dto: LoginUserDto): Promise<AuthEntity> {
+		const user = await prisma.user.findFirst({ where: { dni: dto.dni } });
+		if (!user) throw CustomError.badRequest('Usuario no encontrado');
 
-// 		//* validate password
-// 		const passwordIsValid = BcryptAdapter.compare(dto.password, user.password);
-// 		if (!passwordIsValid) throw CustomError.badRequest('Contraseña incorrecta');
+		//* validate password
+		const passwordIsValid = BcryptAdapter.compare(dto.password, user.password);
+		if (!passwordIsValid) throw CustomError.badRequest('Contraseña incorrecta');
 
-// 		const { password, ...rest } = UserEntity.fromObject(user);
+		const { password, ...rest } = UserEntity.fromObject(user);
 
-// 		//* jwt
-// 		const token = await this.handleToken(user.id);
+		//* jwt
+		const token = await this.handleToken(user.dni);
 
-// 		return new AuthEntity(rest, token);
-// 	}
+		return new AuthEntity(rest, token);
+	}
 
-// 	async renew(id: string): Promise<AuthEntity> {
-// 		const user = await UserModel.findById(id);
-// 		if (!user) throw CustomError.badRequest('Usuario no encontrado');
+	async renew(dni: string): Promise<AuthEntity> {
+		const user = await prisma.user.findFirst({ where: { dni } });
+		if (!user) throw CustomError.badRequest('Usuario no encontrado');
 
-// 		const { password, ...rest } = UserEntity.fromObject(user);
+		const { password, ...rest } = UserEntity.fromObject(user);
 
-// 		//* jwt
-// 		const token = await this.handleToken(user.id);
+		//* jwt
+		const token = await this.handleToken(user.dni);
 
-// 		return new AuthEntity(rest, token);
-// 	}
-// }
+		return new AuthEntity(rest, token);
+	}
+}
