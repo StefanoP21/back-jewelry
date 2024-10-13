d56551dc316282009e415b7581a215d50418beb5
Oscar Medina <omedina.reyes@hotmail.com>
2024-10-12 20:43:31 -0500

    refactor(auth): added user response entity

--- src/features/product/domain/use-cases/index.ts
+++ src/features/product/domain/use-cases/index.ts
@@ -1,5 +1,5 @@
 export { CreateProduct } from './create.use-case';
 export { DeleteProduct } from './delete.use-case';
 export { GetAllProducts } from './getAll.use-case';
-export { GetProductById } from './getbyid.use-case';
+export { GetProductById } from './getById.use-case';
 export { UpdateProduct } from './update.use-case';
